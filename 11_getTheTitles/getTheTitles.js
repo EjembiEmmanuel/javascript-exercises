68a8d53f8a978012077ae9fad7267c0ffe01923e
Ejembi Emmanuel <ejembioche50@gmail.com>
2022-04-01 15:59:34 +0100

    Worked on exercises 11 and 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+  let titles = [];
+  obj.forEach(function(obj) {
+      titles.push(obj["title"]);
+  });
+  return titles;
 };
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,46 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    let oldest = "";
+    let currentAge;
+    let previousAge = 0;
 
+    obj.forEach(function(obj) {
+        if(!obj.yearOfDeath) {
+            currentAge = 2022 - obj.yearOfBirth;
+            previousAge = currentAge;
+            oldest = obj;
+        }
+        if(obj.yearOfDeath) {
+            currentAge = obj.yearOfDeath - obj.yearOfBirth;
+            if(currentAge > previousAge) {
+                previousAge = currentAge;
+                oldest = obj;
+            }
+        }
+        
+    });
+
+    return oldest;
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942, 
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+  console.log(findTheOldest(people).name);
 
 // Do not edit below this line
 module.exports = findTheOldest;
